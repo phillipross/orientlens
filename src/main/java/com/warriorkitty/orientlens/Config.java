d19fcd0b1942d5b8e9f108ea450d0a7b8d29349f
Phillip Ross <phillip.w.g.ross@gmail.com>
2015-09-09 22:17:13 -0400

    Restored original Config class constant values

--- src/main/java/com/warriorkitty/orientlens/Config.java
+++ src/main/java/com/warriorkitty/orientlens/Config.java
@@ -1,9 +1,9 @@
 package com.warriorkitty.orientlens;
 
 public class Config {
-    public static final String DB_URL = "remote:db01/movielens";
-    public static final String DB_USERNAME = "admin";
-    public static final String DB_PASSWORD = "admin";
+    public static final String DB_URL = "remote:localhost/movielens";
+    public static final String DB_USERNAME = "root";
+    public static final String DB_PASSWORD = "Orientdb123!!!";
     public static final Integer DB_POOL_MIN = 1;
     public static final Integer DB_POOL_MAX = 5;
     public static final String MOVIELENS_PATH = "movielens/";
